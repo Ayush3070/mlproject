a7264b7204e4d518bd39ca7e617649fc883166ac
Ayush3070 <ayushborn007@gmail.com>
2025-10-21 19:27:54 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,6 @@
 import sys
+from src.logger import logging
 import logging
-
-
 def error_message_detail(error, error_detail: sys):
     """
     Creates a detailed error message including filename, line number, and error description.
@@ -21,6 +20,13 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("DIvide by Zero")
+#         raise CustomException(e,sys)
 
 
 
